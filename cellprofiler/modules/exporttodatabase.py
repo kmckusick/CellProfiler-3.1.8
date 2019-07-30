fa305a4f1be8d659e51e4e7a4424a6aa911d6e32
Cloud User <ec2-user@ip-172-31-5-215.us-west-1.compute.internal>
2019-07-30 09:54:07 -0700

    Revert "more logging"
    This reverts commit 577b6f85b5162f4d74ff19c1fbb880171a3ab36c.

--- cellprofiler/modules/exporttodatabase.py
+++ cellprofiler/modules/exporttodatabase.py
@@ -3914,10 +3914,6 @@ OPTIONALLY ENCLOSED BY '"' ESCAPED BY '\\\\';
 
 
             logger.info('WELL ID %s' % well_id)
-            xwid = """well_id       = %(well_id)s """ %(locals())
-            logger.info(xwid)
-
-            logger.info(''
             # db_info has bad chars; must manually add database info 
             # into the Analyst properties file
             contents = """
